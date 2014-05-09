00e5ab578cfbb8f4140626d925a6552562149ce6
aziz <aziz@gmail.com>
2014-05-08 21:50:13 -0300

    Front do play
    Commitando novamente o front do gPlay.

--- FrontEnd/src/app/Application.java
+++ FrontEnd/src/app/Application.java
@@ -1,17 +1,16 @@
 package app;
 
+import service.Produto;
+import service.Usuario;
+import swing.ui.DefaultList;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
-import service.Produto;
-import service.Usuario;
-import service.Cotacao;
-import swing.ui.DefaultList;
-
 /**
- * 
+ *
  * @author itakenami
  */
 public class Application {
@@ -19,14 +18,13 @@ public class Application {
 	private static Application instance;
 	public DefaultList listUsuario;
 	public DefaultList listProdutos;
-        public DefaultList listCotacoes;
+	public DefaultList listCotacoes;
 	public Main main;
 
 	private Application() {
 		this.main = new Main();
 		this.listUsuario = new DefaultList(this.main, new Usuario());
 		this.listProdutos = new DefaultList(this.main, new Produto());
-                this.listCotacoes = new DefaultList(this.main, new Cotacao());
 
 	}
 
